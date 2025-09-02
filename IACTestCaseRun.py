cd2c0b079dae47e49ba2b99421dd4e5e3c2f2a19
Steve <steve.gehly@gmail.com>
2025-09-02 21:03:12 +0200

    Priority basic done

--- IACTestCaseRun.py
+++ IACTestCaseRun.py
@@ -516,8 +516,8 @@ def filter_process_measurements(rso_file, sensor_file, meas_file, output_file):
     
     # Loop over objects
     output_dict = {}
-    # obj_id_list = sorted(list(meas_dict.keys()))
-    obj_id_list = [52373, 90000, 91000, 92000, 93000, 94000, 95000, 96000, 97000, 98000, 99000]
+    obj_id_list = sorted(list(meas_dict.keys()))
+    # obj_id_list = [52373, 90000, 91000, 92000, 93000, 94000, 95000, 96000, 97000, 98000, 99000]
     for obj_id in obj_id_list:
         
         if obj_id not in meas_dict:
@@ -1010,7 +1010,7 @@ if __name__ == '__main__':
     
     
     meas_file = os.path.join('data', 'priority_basic_measurement_data_rgazel_10sec_limitvis_multistep.pkl')
-    output_file = os.path.join('data', 'priority_basic_output_batchPo_rgazel_10sec_limitvis_multistep_secondaries.pkl')
+    output_file = os.path.join('data', 'priority_basic_output_batchPo_rgazel_10sec_limitvis_multistep_all.pkl')
     priority_cdm_file = os.path.join('data', 'priority_basic_cdm_batchPo_rgazel_10sec_limitvis_multistep.pkl')
     
     
@@ -1042,10 +1042,10 @@ if __name__ == '__main__':
     
     # process_cdm_output(estimated_rso_file, output_file, priority_cdm_file)
 
-    # generate_case_summary(meas_file, output_file, truth_file)
+    generate_case_summary(meas_file, output_file, truth_file)
     
     
-    plot_risk_metrics(baseline_cdm_file, priority_cdm_file, truth_file)
+    # plot_risk_metrics(baseline_cdm_file, priority_cdm_file, truth_file)
 
 
 
