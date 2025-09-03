02a4a0687f0e64921eff17a7d26e6ebf6c320a18
Steve <steve.gehly@gmail.com>
2025-09-03 23:33:54 +0200

    TIF 0.1 day 4 done

--- IACTestCaseRun.py
+++ IACTestCaseRun.py
@@ -392,7 +392,7 @@ def generate_greedy_measurements_tif(rso_file, sensor_file, visibility_file,
     
     # Process data in 1 day increments
     meas_dict = {}
-    for day in range(2,3):      
+    for day in range(3,4):      
         
         # Load data if needed
         if day > 0:
@@ -523,8 +523,8 @@ def filter_process_measurements(rso_file, sensor_file, meas_file, output_file):
     
     # Loop over objects
     output_dict = {}
-    # obj_id_list = sorted(list(meas_dict.keys()))
-    obj_id_list = [52373, 90000, 91000, 92000, 93000, 94000, 95000, 96000, 97000, 98000, 99000]
+    obj_id_list = sorted(list(meas_dict.keys()))
+    # obj_id_list = [52373, 90000, 91000, 92000, 93000, 94000, 95000, 96000, 97000, 98000, 99000]
     for obj_id in obj_id_list:
         
         if obj_id not in meas_dict:
@@ -1151,15 +1151,15 @@ if __name__ == '__main__':
     # generate_greedy_measurements(estimated_rso_file, sensor_file, visibility_file,
     #                              truth_file, meas_file, reward_fcn)
     
-    # reward_fcn = sensor.reward_renyi_infogain
-    # generate_greedy_measurements_tif(estimated_rso_file, sensor_file, visibility_file,
-    #                                  truth_file, meas_file, reward_fcn)
+    reward_fcn = sensor.reward_renyi_infogain
+    generate_greedy_measurements_tif(estimated_rso_file, sensor_file, visibility_file,
+                                     truth_file, meas_file, reward_fcn)
     
     
     
     # filter_process_measurements(estimated_rso_file, sensor_file, meas_file, output_file)
 
-    process_filter_output(output_file, truth_file)
+    # process_filter_output(output_file, truth_file)
     
     
     # filter_process_meas_and_save(estimated_rso_file, sensor_file, meas_file, output_file)
